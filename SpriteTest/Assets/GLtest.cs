39e4816b1f9b59b824459b06ae0ef3f728858ec1
Kengo Nakajima <kengo.nakajima@gmail.com>
2015-11-26 14:25:13 +0900

    triangle shows up

---
+++ SpriteTest/Assets/GLtest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GLtest : MonoBehaviour {
+
+    private Material m_material;
+
+    void Start () {
+        // マテリアルの生成.
+        /*        
+        m_material = new Material(
+                                  "Shader \"myShader\" {" +
+                                  "  SubShader {" +
+                                  "    Pass {" +
+                                  "       ZWrite Off" +
+                                  "       Cull Off" + 
+                                  "       BindChannels {" +
+                                  "         Bind \"vertex\", vertex Bind \"color\", color" +
+                                  "       }" +
+                                  "    }" +
+                                  "  }" +
+                                  "}"
+                                  );*/
+        m_material = new Material( Shader.Find( "Particles/Alpha Blended" ) );
+        m_material.hideFlags = HideFlags.HideAndDontSave;
+        m_material.shader.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    void OnPostRender() {
+        if (m_material != null) {
+            m_material.SetPass(0);  // マテリアルのパス0を割り当て.
+  
+            GL.PushMatrix();
+            GL.LoadOrtho();   // 2D描画として処理.
+            GL.Begin(GL.TRIANGLES);
+
+            GL.Color(Color.red);
+            GL.Vertex3( 0.0f, 0.0f, 0.0f );
+
+            GL.Color(Color.green);
+            GL.Vertex3(0.0f, 1.0f, 0.0f );
+
+            GL.Color(Color.blue);
+            GL.Vertex3(0.5f, 0.5f, 0.0f );
+
+            GL.End();
+            GL.PopMatrix();
+        }
+    }    
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
---
+++ SpriteTest/Assets/linetest.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+function DrawGLLine(P1:Vector3,P2:Vector3)
+{
+
+    GL.PushMatrix();
+    GL.LoadOrtho();
+    GL.Begin(GL.LINES);
+    GL.Color(Color.red);
+    GL.Vertex(P1);
+    GL.Vertex(P2);
+    GL.End();
+    GL.PopMatrix();        
+}
+
+function Start () {
+
+}
+
+function Update () {
+    DrawGLLine( Vector3(0,0,0), Vector3(100,100,10) );
+
+}
